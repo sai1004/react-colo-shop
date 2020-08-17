f4597b512bab82ed0037f1a064cbdb4728d8ce53
sai1004 <saikiran.bavandla@gmail.com>
2020-08-18 01:15:19 +0530

    commit

--- src/pages/profile/SideBar.js
+++ src/pages/profile/SideBar.js
@@ -19,7 +19,6 @@ const useStyles = makeStyles((theme) => ({
    root: {
       width: "100%",
       maxWidth: 360,
-      backgroundColor: theme.palette.background.paper,
    },
    nested: {
       paddingLeft: theme.spacing(4),
